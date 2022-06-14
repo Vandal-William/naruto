36a549b076c86db305900788e132a117126bd1c8
William <vandal.william@hotmail.com>
2022-06-14 12:44:05 +0200

    adaptation du site pour la mise en place d'un serveur nodeJs

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/*  1. Créer un serveur et réorganiser les fichiers nécessaire au serveur
+    2. Creer une route pour l'accueil 
+    3. Afficher dynamiquement les articles de la section L'histoire des personnage
+    4. créer un lien pour chacque article qui mene a la page story 
+    et affiche sur la page les infos concerner
+    5. lier dynamiquement le css correspondant a la page story
+*/
